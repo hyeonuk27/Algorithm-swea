ac6eddf3bceb800f4518a1a078915047498c5998
hyeonuk <hyeonuk27@gmail.com>
2021-02-23 00:56:52 +0900

    Solve 0222 algorithm problem

--- 0222/SWEA11572/괄호 검사 코드.py
+++ 0222/SWEA11572/괄호 검사 코드.py
@@ -2,9 +2,9 @@ import sys
 sys.stdin = open('input.txt')
 
 # push 함수
-def push(v):
-    if v == '{' or v == '(':
-        stack.append(v)
+def push(t):
+    if t == '{' or t == '(':
+        stack.append(t)
         return
 # pop 함수
 def pop():
@@ -13,21 +13,32 @@ def pop():
     return stack.pop(-1)
 
 for tc in range(1, int(input())+1):
-    t = input()
+    text = input()
     stack = []
-    ans = 1
-
-    for i in t:
-        push(i)
-        if i == ')':
+    ans = 0
+    for t in text:
+        push(t)
+        # 오른쪽 괄호를 만난 경우 pop하여 정상 / 오류 검사
+        if t == ')':
             pop1 = pop()
-            if pop1 == ')':
-                ans = 0
-        elif i == '}':
-            pop2 = pop()
-            if pop2 == '}':
+            # 정상 괄호
+            if pop1 == '(':
+                ans = 1
+            # 오류 괄호 (pop과 다르거나 빈 경우)
+            else:
                 ans = 0
 
+        # 오른쪽 괄호를 만난 경우 pop하여 정상 / 오류 검사
+        elif t == '}':
+            pop2 = pop()
+            # 정상 괄호
+            if pop2 == '{':
+                ans = 1
+            # 오류 괄호 (pop과 다르거나 빈 경우)
+            else:
+                ans = 0
+
+    # 마지막까지 조사 이후 남아있으면 오류
     if stack:
         ans = 0
     print('#%d %d' % (tc, ans))
