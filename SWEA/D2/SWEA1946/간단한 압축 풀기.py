69228d2e646c33e099133966a55165ab44d63afd
hyeonuk <hyeonuk27@gmail.com>
2021-03-08 22:56:29 +0900

    Solve 0308 algorithm problem

--- SWEA/D2/SWEA1946/간단한 압축 풀기.py
+++ SWEA/D2/SWEA1946/간단한 압축 풀기.py
@@ -2,3 +2,12 @@ import sys
 sys.stdin = open('input.txt')
 
 for t in range(1, int(input())+1):
+    n = int(input())
+    ans = ''
+    for _ in range(n):
+        char, num = input().split()
+        ans += char * int(num)
+
+    print('#%d' % t)
+    for i in range(0, len(ans), 10):
+        print(ans[i:i+10])
