d8b25d86232e3b1f81fb99fc1fcea909c46e246a
hyeonuk <hyeonuk27@gmail.com>
2021-04-08 22:31:08 +0900

    Solve 0408 algorithm problem

--- 0405/SWEA11746/이진탐색트리 - 생성.py
+++ 0405/SWEA11746/이진탐색트리 - 생성.py
@@ -1,3 +1,31 @@
 import sys
 sys.stdin = open('input.txt')
 
+# 이진탐색트리 생성
+def make_BST(pn, cn):
+    if pn > cn: # 좌
+        if tree[pn][0]:
+            return make_BST(tree[pn][0], cn)
+        tree[pn][0] = cn
+    else: # 우
+        if tree[pn][1]:
+            return make_BST(tree[pn][1], cn)
+        tree[pn][1] = cn
+
+# 전위순회
+def preorder(n):
+    if n:
+        ans.append(n)
+        preorder(tree[n][0])
+        preorder(tree[n][1])
+
+N = int(input())
+nodes = list(map(int, input().split()))
+tree = [[0, 0] for _ in range(max(nodes)+1)]
+root = nodes[0]
+for i in range(1, N):
+    make_BST(root, nodes[i])
+
+ans = []
+preorder(root)
+print('-'.join(map(str, ans)))
